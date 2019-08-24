2d3ea4d2b74c8c5900a2cab6550e0e575893d0dc
Yagnesh Modh <yagnesh.ash.modh@gmail.com>
2019-08-24 10:43:56 +0530

    First Commit

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "react-native/react-native": true,
+    },
+    parser: "babel-eslint",
+    "extends": [
+        "airbnb",
+        "prettier",
+        "plugin:react/recommended",
+        "prettier/react"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "babel",
+        "import",
+        "jsx-a11y",
+        "prettier",
+        "react",
+        "react-native",
+        "react-hooks"
+    ],
+    "rules": {
+        "react/jsx-filename-extension": [
+            2,
+            {
+              extensions: [".js", ".jsx"]
+            }
+          ],
+          "prettier/prettier": [2]
+    }
+};
